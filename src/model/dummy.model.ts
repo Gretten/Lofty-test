a031f65ca05b7100ea991446d9ad2491149f4d19
gretten <any@anykey.com>
2020-10-17 18:20:12 +0300

    An interface for fake data items is created

---
+++ src/model/dummy.model.ts
@@ -0,0 +1,8 @@
+export interface DummyDataItem {
+    name: string;
+    brand: string | null;
+    weight: number | null;
+    quantity: number | null;
+    price: number | null;
+    stock: number | null;
+}
